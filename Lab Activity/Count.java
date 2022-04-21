76621be155ed428c690030c37ad2067ce1467d3b
MugeshP <96114083+MugeshP@users.noreply.github.com>
2022-04-21 14:03:30 +0530

    Add files via upload

---
+++ Lab Activity/Count.java
@@ -0,0 +1,17 @@
+public class Count{
+
+  public static void main(String[] args) {
+
+    int count = 0, num = 64183452;
+
+    while (num != 0) 
+	{
+      num /= 10;
+      ++count;
+    }
+
+    System.out.println("Number of digits: " + count);
+  }
+}
+
+
